5db05bb472ae5e1ac7411d3ef6f1f04ed314e4d2
inesmrr <imrivera@iu.edu>
2023-10-11 09:28:56 -0400

    health system

---
+++ YGR_game/Assets/Scripts/HeartSystem.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartSystem : MonoBehaviour
+{
+    public GameObject[] hearts;
+    private int life;
+    private bool dead;
+
+
+    void Start()
+    {
+        //life is the amount of hearts we have
+        life = hearts.Length;
+    }
+
+
+    void Update()
+    {
+        if (dead == true)
+        {
+            //dead code, TO DO: trigger game over screen
+            Debug.Log("we died");
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //take -1 if we have hearts
+        if (life >= 1)
+        {
+            life -= damage;
+            Destroy(hearts[life].gameObject);
+            //if we have no hearts
+            if (life < 1) 
+            {
+                //we died
+                dead = true;
+            }
+        }
+    }
+}
