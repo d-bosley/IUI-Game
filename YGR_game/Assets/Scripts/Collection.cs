126f49f002e029b568d3d43216f9bed4809878ce
inesmrr <imrivera@iu.edu>
2023-10-18 12:44:01 -0400

    coin counter hooked to UI

--- YGR_game/Assets/Scripts/Collection.cs
+++ YGR_game/Assets/Scripts/Collection.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Collection : MonoBehaviour
 {
+    private int coins = 0;
+
+    public TMP_Text CoinsTxt; //to have access to UI coin counter
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //if object we entered has collectible tag...
         if (collision.gameObject.CompareTag("Collectible"))
         {
-            Destroy(collision.gameObject);
-            print("collected");
+            Destroy(collision.gameObject); //destroy it
+            coins += 1; //increment counter
+            Debug.Log("coins: " + coins);
+            CoinsTxt.text = "Coins: " + coins; //update UI
         }
     }
 }
